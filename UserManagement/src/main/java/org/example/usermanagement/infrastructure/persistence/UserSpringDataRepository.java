59f5ac49cbc2df8ac3b0180ad7ea4dc92b474e6e
Piccolo7210 <mustakim.mohsin7210@gmail.com>
2025-07-10 03:55:18 +0600

    ada

--- UserManagement/src/main/java/org/example/usermanagement/infrastructure/persistence/UserSpringDataRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.usermanagement.infrastructure.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface UserSpringDataRepository extends JpaRepository<UserJpaEntity, UUID> {
-}
